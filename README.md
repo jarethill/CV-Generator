392c01009b62b283dad773988da2ef90e54a3eb3
Jaret Hill <jaret.hill@tuta.io>
2020-10-08 07:39:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV Generator
+
+Currently this just collects info to be used in a CV. May update with a back end to fill out a template CV document and return it to the user for download
---
+++ src/components/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import GeneralInformation from './GeneralInformation';
+import Education from './Education';
+import Experience from './Experience';
+import '../styles/App.scss';
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.updateState = this.updateState.bind(this);
+
+        this.state = {
+            general: {
+                firstName: '',
+                lastName: '',
+                addressOne: '',
+                addressTwo: '',
+                city: '',
+                state: '',
+                zip: '',
+            },
+            education: {
+                schools: [
+                    {
+                        schoolName: '',
+                        schoolTitle: '',
+                        dateFrom: '',
+                        dateTo: '',
+                    },
+                ],
+            },
+            experience: {
+                companies: [
+                    {
+                        companyName: '',
+                        positionTitle: '',
+                        jobTasks: [''],
+                        dateFrom: '',
+                        dateTo: '',
+                    },
+                ],
+            },
+        };
+    }
+
+    updateState(property, data) {
+        this.setState({
+            [property]: data,
+        });
+
+        setTimeout(() => {
+            console.log(this.state);
+        }, 1000);
+    }
+    render() {
+        const { general, education, experience } = this.state;
+
+        return (
+            <section id='app'>
+                <Header />
+                <GeneralInformation updateState={this.updateState} info={general} rootName='general' />
+                <Education updateState={this.updateState} info={education} rootName='education' />
+                <Experience updateState={this.updateState} info={experience} rootName='experience' />
+            </section>
+        );
+    }
+}
+
+export default App;
